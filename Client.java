3785fc0116218f5216d5d680a9ae453b663e395a
jcmeier22@gmail.com <jcmeier22@gmail.com>
2016-03-10 13:18:26 -0500

    Comments and client text scanner added

--- Client.java
+++ Client.java
@@ -1,22 +1,49 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Client {
     
+    //Variables used in Client class
+    private String clientInput;
+    
+    //Constructor for initializing Client objects
+    public Client(String input){
+        clientInput = input;
+    }
+    
+    // Method run() used for setting up and running the client
     public void run() throws IOException {
-        Socket sock = new Socket("localhost", 444);
-        PrintStream print = new PrintStream(sock.getOutputStream());
-        print.println("Hey");
         
+        //Sets up client socket to connect to localhost on port 444
+        Socket sock = new Socket("localhost", 444);
+        
+        //Prints input message from constructor to output stream
+        PrintStream print = new PrintStream(sock.getOutputStream());
+        print.println(clientInput);
+        
+        //Sets up buffered input reader to read data from input stream
         InputStreamReader reader = new InputStreamReader (sock.getInputStream());
         BufferedReader clientreader = new BufferedReader(reader);
         
-        String clientmessage = clientreader.readLine();
-        System.out.println(clientmessage);
+        //Makes string called serverMessage equal to data sent from server stream
+        //Prints serverMessage 
+        String serverMessage = clientreader.readLine();
+        System.out.println(serverMessage);
+        
+        //Closes client socket
+        sock.close();
     }
     
     public static void main(String[] args) throws IOException {
-        Client client = new Client();
+        
+        //Solicits input from client user with a scanner then closes scanner
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        scan.close();
+        
+        //Creates new client and calls run() method
+        Client client = new Client(input);
         client.run();
     }
 
--- Server.java
+++ Server.java
@@ -2,21 +2,30 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-
+    
     public void run() throws IOException {
+
+        /* Creates server socket on port 444 and listens for 
+           client connection requests 
+        */
         ServerSocket servsock = new ServerSocket(444);
         Socket sock = servsock.accept();
         
+        // Sets up a buffered input reader to read data from input stream
         InputStreamReader input = new InputStreamReader(sock.getInputStream());
         BufferedReader reader = new BufferedReader(input);
         
+        //Makes a message string equal to message sent from client
         String message = reader.readLine();
-        System.out.println(message);
         
+        //If client message is not null, echoes message by printing to output stream
         if(message != null){
             PrintStream print = new PrintStream(sock.getOutputStream());
-            print.println("Message recieved");
+            print.println("Message recieved: " + message);
         }
+        
+        //Closes server socket
+        servsock.close();
     }
 	
     public static void main(String[] args) throws IOException {
