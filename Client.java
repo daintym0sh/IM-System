5145391dee73f6fe24f9d0b1d695caa2707a8efb
jcmeier22@gmail.com <jcmeier22@gmail.com>
2016-04-02 20:35:13 -0400

    GUI added

--- Client.java
+++ Client.java
@@ -1,51 +1,83 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
-import java.util.*;
+import javax.swing.*;
 
-public class Client {
-    
+public class Client extends JFrame {
+
     //Variables used in Client class
-    private String clientInput;
-    
+    private Socket sock;
+    private InputStreamReader input;
+    private BufferedReader clientreader;
+    private String serverMessage;
+    private JTextField clientText;
+    private JTextArea clientWindow;
+
     //Constructor for initializing Client objects
-    public Client(String input){
-        clientInput = input;
+    public Client(){
+        super("CLIENT");
+        clientText = new JTextField();
+        clientText.addActionListener(
+                new ActionListener(){
+                public void actionPerformed(ActionEvent event){
+                    try {
+                        sendMessage(event.getActionCommand());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    clientText.setText("");
+                }
+                }
+        );
+        add(clientText, BorderLayout.SOUTH);
+        clientWindow = new JTextArea();
+        add(new JScrollPane(clientWindow));
+        setSize(300, 150); 
+        setVisible(true);
     }
-    
+
     // Method run() used for setting up and running the client
     public void run() throws IOException {
-        
+        try{
+            connectServer();
+            chatting();
+        }finally{
+            closeOut();
+        }
+    }
+    
+    private void connectServer() throws IOException {
         //Sets up client socket to connect to localhost on port 444
-        Socket sock = new Socket("localhost", 444);
-        
-        //Prints input message from constructor to output stream
-        PrintStream print = new PrintStream(sock.getOutputStream());
-        print.println(clientInput);
+        sock = new Socket("localhost", 444);
         
         //Sets up buffered input reader to read data from input stream
-        InputStreamReader reader = new InputStreamReader (sock.getInputStream());
-        BufferedReader clientreader = new BufferedReader(reader);
-        
+        input = new InputStreamReader(sock.getInputStream());
+        clientreader = new BufferedReader(input);
+    }
+    
+    private void chatting() throws IOException {
         //Makes string called serverMessage equal to data sent from server stream
         //Prints serverMessage 
-        String serverMessage = clientreader.readLine();
-        System.out.println(serverMessage);
-        
-        //Closes client socket
+        serverMessage = clientreader.readLine();
+        clientWindow.append("\nSERVER: " + serverMessage);
+    }
+
+    private void sendMessage(String message) throws IOException {
+        PrintStream print = new PrintStream(sock.getOutputStream());
+        print.println(message);
+        clientWindow.append("\nCLIENT: " + message);
+    }
+    
+    private void closeOut() throws IOException {
         sock.close();
     }
     
     public static void main(String[] args) throws IOException {
-        
-        //Solicits input from client user with a scanner then closes scanner
-        System.out.println("Enter a message to send to the server:");
-        Scanner scan = new Scanner(System.in);
-        String input = scan.next();
-        scan.close();
-        
         //Creates new client and calls run() method
-        Client client = new Client(input);
+        Client client = new Client();
+        client.setDefaultCloseOperation(EXIT_ON_CLOSE);
         client.run();
     }
-
 }
--- Server.java
+++ Server.java
@@ -1,35 +1,75 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
+import javax.swing.*;
 
-public class Server {
+public class Server extends JFrame {
+    
+    private ServerSocket servsock;
+    private Socket sock;
+    private InputStreamReader input;
+    private BufferedReader reader;
+    private String clientMessage;
+    private JTextField serverText;
+    private JTextArea serverWindow;
+    
+    public Server(){
+        super("SERVER");
+        serverText = new JTextField();
+        serverText.addActionListener(
+                new ActionListener(){
+                public void actionPerformed(ActionEvent event){
+                    try {
+                        sendMessage(event.getActionCommand());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    serverText.setText("");
+                }
+            }
+        );
+        add(serverText, BorderLayout.SOUTH);
+        serverWindow = new JTextArea();
+        add(new JScrollPane(serverWindow));
+        setSize(300, 150); 
+        setVisible(true);
+    }
     
     public void run() throws IOException {
-
-        /* Creates server socket on port 444 and listens for 
-           client connection requests 
-        */
-        ServerSocket servsock = new ServerSocket(444);
-        Socket sock = servsock.accept();
-        
-        // Sets up a buffered input reader to read data from input stream
-        InputStreamReader input = new InputStreamReader(sock.getInputStream());
-        BufferedReader reader = new BufferedReader(input);
-        
-        //Makes a message string equal to message sent from client
-        String message = reader.readLine();
-        
-        //If client message is not null, echoes message by printing to output stream
-        if(message != null){
-            PrintStream print = new PrintStream(sock.getOutputStream());
-            print.println("Message recieved: " + message);
+        try{
+            /* Creates server socket on port 444 and listens for 
+            client connection requests 
+             */
+             servsock = new ServerSocket(444);
+             sock = servsock.accept();
+             
+             // Sets up a buffered input reader to read data from input stream
+             input = new InputStreamReader(sock.getInputStream());
+             reader = new BufferedReader(input);
+             
+             //Makes a message string equal to message sent from client
+             clientMessage = reader.readLine();
+             serverWindow.append("\nCLIENT: " + clientMessage);
+        }finally{
+            closeOut();
         }
-        
-        //Closes server socket
+    }
+    
+    private void sendMessage(String message) throws IOException {
+        PrintStream print = new PrintStream(sock.getOutputStream());
+        print.println(message);
+        serverWindow.append("\nSERVER: " + message);
+    }
+    
+    private void closeOut() throws IOException {
         servsock.close();
     }
 	
     public static void main(String[] args) throws IOException {
          Server server = new Server();
+         server.setDefaultCloseOperation(EXIT_ON_CLOSE);
          server.run();
      }
 }
